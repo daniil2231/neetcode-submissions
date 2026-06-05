389c63fad0b6cc88ab2612d519e77592b040ec47
Daniil <66083382+daniil2231@users.noreply.github.com>
2026-06-05 19:54:09 +0200

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        
+        wordList.append(beginWord)
+        adj = {x: set() for x in wordList}
+        for i in range(len(wordList)):
+            for j in range(len(wordList[i])):
+                newWord1 = wordList[i][:j] + "#" + wordList[i][j + 1:]
+
+                for k in range(len(wordList)):
+                    if i != k:
+                        newWord2 = wordList[k][:j] + "#" + wordList[k][j + 1:]
+
+                        if newWord1 == newWord2:
+                            adj[wordList[i]].add(wordList[k])
+                            adj[wordList[k]].add(wordList[i])
+        
+        q = deque([[beginWord, 1]])
+        seen = set([beginWord])
+        while q:
+            node, transformations = q.popleft()
+
+            if node == endWord:
+                return transformations
+
+            for neighbor in adj[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    q.append([neighbor, transformations + 1])
+        
+        return 0
